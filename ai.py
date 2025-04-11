84e74c9c5dfc7e0543ccc104d9919b097e617840
hagersameh111 <126250861+hagersameh111@users.noreply.github.com>
2025-04-12 00:11:21 +0200

    Updates

--- ai.py
+++
@@ -1,6 +0,0 @@
-import os
-import shutil
-from sklearn.model_selection import train_test_split
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
--- src/components/Products/Pd.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Pd = () => {
-  return (
-    <div>Pd</div>
-  )
-}
-
-export default Pd
